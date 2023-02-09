33a38ea5d520e1c8de11651a408c90e2abd9e57b
Anderson Santana <67117035+andersonssantana@users.noreply.github.com>
2023-02-09 11:42:07 -0300

    Gabarito

--- src/script.js
+++ src/script.js
@@ -1,8 +1,31 @@
-const callEndpoint = () => {
+const callEndpoint = (ddd, callback) => {
+  const url = `https://brasilapi.com.br/api/ddd/v1/${ddd}`;
+  fetch(url)
+    .then((request) => {
+      if (request.ok) {
+        return request.json();
+      }
+      throw new Error('ihhhh deu ruim');
+    })
+    .then((data) => callback(data))
+    .catch((error) => console.error(error.message));
 };
 
-const generateResult = () => {
+const generateResult = ({ state, cities }) => {
+  const listContainer = document.querySelector('#api-result');
+  const title = document.querySelector('h1');
+  title.innerText = `Cidades do ${state} - ${cities.length} cidades`;
+  cities.forEach((el) => {
+    const line = document.createElement('tr');
+    const column = document.createElement('td');
+    column.innerText = el;
+    line.appendChild(column);
+    listContainer.appendChild(line);
+  });
 };
+
+const cityCode = 15;
 
 window.onload = () => {
+  callEndpoint(cityCode, generateResult);
 };
