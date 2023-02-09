f9a2db136b5d77de9b3057e3d04ccfa6c1cc3f49
Anderson Santana <67117035+andersonssantana@users.noreply.github.com>
2023-02-09 19:45:56 -0300

    Turma 29

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
 const callEndpoint = () => {
+  // consumir api
+  // https://brasilapi.com.br/api/ddd/v1/"??"
 };
 
 const generateResult = () => {
+  // transformar os dados da api em coisas que front-end faz
 };
 
 window.onload = () => {
+  // chamar tudo isso quando a página carregar
 };
