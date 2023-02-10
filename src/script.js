4f8c97e999870964356863c39298dafa743e2ac7
Anderson Santana <67117035+andersonssantana@users.noreply.github.com>
2023-02-10 14:19:01 -0300

    Exercício resolvido

--- src/script.js
+++ src/script.js
@@ -1,12 +1,35 @@
-const callEndpoint = () => {
-  // consumir api
-  // https://brasilapi.com.br/api/ddd/v1/"??"
+const callEndpoint = (ddd, callback) => {
+  const BASE_URL = `https://brasilapi.com.br/api/ddd/v1/${ddd}`;
+
+  fetch(BASE_URL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Erro da API: Ihhh deu ruim');
+    })
+    .then((data) => callback(data))
+    .catch((error) => console.error(error.message));
 };
 
-const generateResult = () => {
-  // transformar os dados da api em coisas que front-end faz
+const generateResult = ({ cities, state }) => {
+  const tableContainer = document.querySelector('#api-result');
+  const title = document.querySelector('h1');
+
+  title.innerText = `Cidades do estado ${state} - ${cities.length} cidades`;
+
+  cities.forEach((city) => {
+    const line = document.createElement('tr');
+    const column = document.createElement('td');
+
+    column.innerText = city;
+    line.appendChild(column);
+    tableContainer.appendChild(line);
+  });
 };
+
+const cityCode = 31;
 
 window.onload = () => {
-  // chamar tudo isso quando a página carregar
+  callEndpoint(cityCode, generateResult);
 };
